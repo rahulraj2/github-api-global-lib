9cc39779aee6e5c0a91fcf9ef20520045ecd368c
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-14 02:15:33 +0400

    Added more condition

--- vars/callingOtherMethod.groovy
+++ vars/callingOtherMethod.groovy
@@ -2,7 +2,9 @@ def call(Map config = [:]) {
     echo "${config.age}"
     if(config.age == null){
         echo "Value Not Passed !! Please Pass the value"
-    }else{
+    }else if(config.age > 20) {
         echo "You are adult"
+    }else{
+        echo "Not Authorize"
     }
 }
