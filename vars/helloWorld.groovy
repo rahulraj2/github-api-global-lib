91a2283b39a4da418843559220520179014eb2eb
Rahul Raj <40715480+rahulraj2@users.noreply.github.com>
2022-08-14 01:18:11 +0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,11 @@
 def call(Map config = [:]) {
     echo "Hello World From Shared Library"
+    pipeline{
+        agent any
+        stages{
+            stage('Hello From Inside'){
+                echo "${config.name}"
+            }
+        }
+    }
 }
