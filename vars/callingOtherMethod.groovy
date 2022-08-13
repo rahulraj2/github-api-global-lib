1b939ed0c4edffabe0f346c88e76084f62ad5de5
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-14 02:05:40 +0400

    checking for null

--- vars/callingOtherMethod.groovy
+++ vars/callingOtherMethod.groovy
@@ -1,8 +1,8 @@
 def call(Map config = [:]) {
     echo "${config.age}"
-    if(config.age > 18){
-        echo "You are adult"
-    }else{
+    if(config.age == null){
         echo "Value Not Passed !! Please Pass the value"
+    }else{
+        echo "You are adult"
     }
 }
