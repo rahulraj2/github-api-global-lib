e46e2e86d66a27c3733e2b44a705f4402d328c7b
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-14 23:54:32 +0400

    added 2 paramter

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,10 +1,13 @@
 def call(Map config) {
+    def a, b , c
     pipeline{
         agent any
         stages{
             stage('Hello From Inside'){
                 steps{
                     script{
+                        a = 10 b=20
+                        echo "${a} ${b}"
                         echo "Hello World From Shared Library ${config.application}"
                         if(config.name == null){
                             echo "Value Not Provided"
