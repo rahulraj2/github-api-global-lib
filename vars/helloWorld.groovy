d8caf2579412a24c843e1b5d063ec43cfcddbf39
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-16 20:39:20 +0400

    Removed variable

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -17,7 +17,6 @@ def call(Map config) {
                         println BRANCH_NAME
                         println BUILD_EXECUTED_BY
                         println env.BUILD_EXECUTED_BY
-                        println SOME_NUMBER
                         echo "Hello World From Shared Library ${config.application}"
                         if(config.name == null){
                             echo "Value Not Provided"
