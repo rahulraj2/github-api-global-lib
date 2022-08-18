e2453f534a609a8359dc4b7091a48ea6dabb326f
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-19 02:10:15 +0400

    generating display name

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -5,6 +5,9 @@ def call(Map config) {
         environment {
             DOCKER_CRED = credentials('dockercred')
             BUILD_EXECUTED_BY = "Rahul"
+            GIT_URL = "${GIT_URL}"
+            GIT_COMMIT = "${GIT_COMMIT}".substring(0,7)
+            BUILD_DISPLAY_NAME = "${BUILD_NUMBER}"+"-${GIT_COMMIT}-"+"${BRANCH_NAME}"
         }
         stages{
             stage('Hello From Inside'){
@@ -17,6 +20,8 @@ def call(Map config) {
                         println BRANCH_NAME
                         println BUILD_EXECUTED_BY
                         println env.BUILD_EXECUTED_BY
+                        println env.BUILD_DISPLAY_NAME
+                        
                         echo "Hello World From Shared Library ${config.application}"
                         if(config.name == null){
                             echo "Value Not Provided"
