9248f6f68c933cce842ffc59f5b75c5554fe41a8
Rahul Raj <Rahulraj21017@gmail.com>
2022-08-19 11:55:50 +0400

    Paramter reversed

--- vars/callingOtherMethod.groovy
+++ vars/callingOtherMethod.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     bat "docker login -u ${DOCKER_CRED_USR} -p ${DOCKER_CRED_PSW}"
     echo "${env.BUILD_EXECUTED_BY}"
     echo "${config.age}"
-    if((config.ENV_NAMESPACE).indexof(NAMESPACE)){
+    if((config.ENV_NAMESPACE).indexof(ENV_NAMESPACE)){
         echo "${config.ENV_NAMESPACE} comes under array"
     }else{
         echo "Please get your namespace added from Inside method"
